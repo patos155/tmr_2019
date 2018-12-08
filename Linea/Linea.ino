90ff76b7f51a8816ae8db98715f1392575743fde
israellh2001 <israellh2001@hotmail.com>
2018-12-08 09:47:58 -0600

    Cambio de motor shield

--- Linea/Linea.ino
+++ Linea/Linea.ino
@@ -1,6 +1,6 @@
-#include <ArduinoMotorShieldR3.h>
-
-ArduinoMotorShieldR3 motores;
+#include <AFMotor.h>
+AF_DCMotor motor_derecho(2);   //motor derecho
+AF_DCMotor motor_izquierdo(3);   //motor izquierdo
 
 //localizacion de los pines de los sensores
 //sensor infrarojo
@@ -13,11 +13,11 @@ int s5_ = 52;
 
 //variables para almacenar los valores
 //sensor infrarojo
-int s1;
-int s2;
-int s3;
-int s4;
-int s5;
+int l1=1;
+int l2=1;
+int l3=0;
+int l4=1;
+int l5=1;
 
 
 int neg = 0;
@@ -34,41 +34,15 @@ Serial.begin(9600);
 }
 
 void loop() {
+  //Lectura de los sensores de infrajos
+   l5=digitalRead(s5_);
+   l4=digitalRead(s4_);
+   l3=digitalRead(s3_);
+   l2=digitalRead(s2_);
+   l1=digitalRead(s1_);
+       motor_derecho.setSpeed(75);//210
+       motor_derecho.run(FORWARD);
+       motor_izquierdo.setSpeed(75);
+       motor_izquierdo.run(FORWARD);
 
-   //Lectura de los sensores de infrajos
-   s5=digitalRead(s5_);
-   s4=digitalRead(s4_);
-   s3=digitalRead(s3_);
-   s2=digitalRead(s2_);
-   s1=digitalRead(s1_);
-
-  if((s1==bco && s2==bco && s4==bco && s5==bco)){
-    Moverse(100,100);
-      //Serial.println("1");
-
-   } 
-  if(s2 == neg && s1 == neg){
-     Moverse(100,0);
-       //Serial.println("2");
-
-   }
-   if(s4 == neg && s5 == neg){
-    Moverse(0,100);
-    //Serial.println("3");
-   }
-   if(s2 == neg){
-    Moverse(100,0);
-    //Serial.println("4");
-   }
-   if(s4 == neg){
-    Moverse(0,100);
-    //Serial.println("5");
-    }
- //delay(1000);
 }
-   
-int Moverse(int velI, int velD){
-  motores.setM1Speed(velI);
-  motores.setM2Speed(velD);  
-}
-   
