a80763503830ed749e2199da179f89c8e90b547e
JuanMGD <33186388+JuanMGD@users.noreply.github.com>
2019-03-19 14:31:27 -0600

    código para probar el emisor

---
+++ Mayor/prueba_emisor/prueba_emisor.ino
@@ -0,0 +1,36 @@
+#include "RF24.h"
+RF24 myRadio (7, 8); // in Mega can use> (48, 49); 
+byte addresses[][6] = {"0"}; 
+
+struct package
+{
+  int id=1;
+  float temperature = 18.3;
+  char  text[100] = "Text to be transmitted";
+};
+typedef struct package Package;
+Package data;
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(100);
+  myRadio.begin();  
+  myRadio.setChannel(115); 
+  myRadio.setPALevel(RF24_PA_MAX);
+  myRadio.setDataRate( RF24_250KBPS ) ; 
+  myRadio.openWritingPipe( addresses[0]);
+}
+
+void loop()
+{
+  myRadio.write(&data, sizeof(data)); 
+  Serial.print("\nPackage:");
+  Serial.print(data.id);
+  Serial.print("\n");
+  Serial.println(data.temperature);
+  Serial.println(data.text);
+  data.id = data.id + 1;
+  data.temperature = data.temperature+0.1;
+  delay(50);
+}
